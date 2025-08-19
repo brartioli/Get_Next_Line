167fa51ce359c908689c527746a3e5bb1f163d21
Bruna Fernandez Artioli <bfernan2@student.42sp.org.br>
2025-08-19 14:58:22 -0300

    feat: ok

--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,26 @@
 /*   By: bfernan2 <bfernan2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 11:56:48 by bfernan2          #+#    #+#             */
-/*   Updated: 2025/08/15 18:37:51 by bfernan2         ###   ########.fr       */
+/*   Updated: 2025/08/19 14:38:24 by bfernan2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdint.h>
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 1024
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
 char	*get_next_line(int fd);
 int		find_nl(char *str);
+char	*ft_extract_line(char *buffer);
+char	*ft_strjoin(char *s1, char *s2);
+void	*ft_calloc(size_t nmemb, size_t size);
+size_t	ft_strlen(const char *s);
 
 #endif
